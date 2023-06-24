41e695e167e293030b7d5238f7aba000450d6ff3
Vedant <vedantnp200@gmail.com>
2023-06-24 22:37:24 +0530

    [Vedant]-CRUD Operation - Insert Records into Employee_Payroll

--- Employee-Payroll.sql
+++ Employee-Payroll.sql
@@ -7,10 +7,19 @@ SELECT name FROM sys.databases;
 use Payroll_Service;
 
 -- UC 2: create employee payroll table
-
+/*
 CREATE TABLE Employee_Payroll(
 	ID INT IDENTITY(1,1) PRIMARY KEY,
 	Name VARCHAR(50),
 	Salary FLOAT,
 	Start_Date DATE
 );
+*/
+--CRUD Operations
+-- UC 3: Insert Records
+
+INSERT INTO Employee_Payroll 
+VALUES 
+	('Chris Evans', 55000.23, '01-04-2022'),
+	('Paul Rudd', 55000.23, '06-04-1969'),
+	('Sebastian Stan', 55000.23, '13-08-1982');
