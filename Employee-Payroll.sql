99d065e7de19e13dd8ea8cf2b9baa67d1b96833f
Vedant <vedantnp200@gmail.com>
2023-06-24 15:58:24 +0530

    [Vedant]-create employee payroll table

--- Employee-Payroll.sql
+++ Employee-Payroll.sql
@@ -5,3 +5,14 @@ CREATE DATABASE Payroll_Service;
 SELECT name FROM sys.databases;
 
 use Payroll_Service;
+
+-- UC 2: create employee payroll table
+
+CREATE TABLE Employee_Payroll(
+	ID INT IDENTITY(1,1) PRIMARY KEY,
+	Name VARCHAR(50),
+	Salary FLOAT,
+	Start_Date DATE
+);
+
+SELECT * FROM Employee_Payroll;
