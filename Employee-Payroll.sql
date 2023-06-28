3c1a218e345436c35278ade1b79a01689ddeff0e
Vedant <vedantnp200@gmail.com>
2023-06-28 12:30:15 +0530

    [Vedant]-find sum, average, min, max and number of male and female employees

--- Employee-Payroll.sql
+++ Employee-Payroll.sql
@@ -24,6 +24,9 @@ VALUES
 	('Paul Rudd', 43789.70, '2018-06-04'),
 	('Sebastian Stan', 67834.10, '2013-03-08');
 
+INSERT INTO Employee_Payroll VALUES ('Brie Larson', 67234.67, '2020-08-15', 'F');
+INSERT INTO Employee_Payroll VALUES ('Hayley Atwell', 89123.90, '2019-12-01', 'F');
+
 -- UC 4: retrieve all the employee payroll table
 
 SELECT * FROM Employee_Payroll;
@@ -43,3 +46,27 @@ ADD Gender CHAR(1) CHECK(Gender IN ('M', 'F'));
 
 UPDATE Employee_Payroll SET Gender = 'M'
 WHERE name = 'Chris Evans' or name = 'Paul Rudd' or name = 'Sebastian Stan';
+
+--UC 7: find sum, average, min, max and number of male and female employees
+
+SELECT SUM(Salary) AS Salary_Of_Females FROM Employee_Payroll
+WHERE Gender = 'F'
+GROUP BY Gender;
+
+SELECT SUM(Salary) AS Salary_Of_Males FROM Employee_Payroll
+WHERE Gender = 'M'
+GROUP BY Gender;
+
+SELECT AVG(Salary) AS Average_Salary FROM Employee_Payroll;
+
+SELECT MIN(Salary) AS Minimum_Salary FROM Employee_Payroll;
+
+SELECT MAX(Salary) AS Maximum_Salary FROM Employee_Payroll;
+
+SELECT COUNT(Gender) AS Count_Males FROM Employee_Payroll
+WHERE Gender = 'M'
+GROUP BY Gender;
+
+SELECT COUNT(Gender) AS Count_Females FROM Employee_Payroll
+WHERE Gender = 'F'
+GROUP BY Gender;
